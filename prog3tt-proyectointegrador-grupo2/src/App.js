c07ceab3960a783ce11aff82c09550321ef2c375
aaugier@udesa.edu.ar <aaugier@udesa.edu.ar>
2023-09-04 15:48:21 -0300

    app.js

--- prog3tt-proyectointegrador-grupo2/src/App.js
+++ prog3tt-proyectointegrador-grupo2/src/App.js
@@ -1,17 +1,22 @@
-
 import React from "react";
-import CardSerie from "./components/CardSerie/CardSerie";
-import Card from "./components/Card/Card";
-
+import { Switch, Route } from "react-router-dom";
+import Home from "./screen/Home/Home";
+import Detail from "./screen/Detail/Detail";
+import Buscador from "./screen/Buscador/Buscador";
+import Favoritos from "./screen/Favoritos/Favoritos";
+import NoEncontrada from "./screen/NoEcontrada/NoEncontrada";
 
 function App() {
-  const peliculas = [{
-    image: '',
-    name: '',
-    info: ''
-  }]
   return (
-    <p>React</p>
+    <React.Fragment>
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/Detail" component={Detail} />
+        <Route path="/Favoritos" exact={true} component={Favoritos} />
+        <Route path="/Buscador/:id" component={Buscador} />
+        <Route path="" component={NoEncontrada} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
