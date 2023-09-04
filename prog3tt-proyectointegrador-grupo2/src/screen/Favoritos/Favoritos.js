ba9d74aeac48e740c568f7aab046a8e94f970785
aaugier@udesa.edu.ar <aaugier@udesa.edu.ar>
2023-09-04 16:56:39 -0300

    detalles

--- prog3tt-proyectointegrador-grupo2/src/screen/Favoritos/Favoritos.js
+++ prog3tt-proyectointegrador-grupo2/src/screen/Favoritos/Favoritos.js
@@ -1,4 +1,6 @@
-import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import React from "react";
+import Container from "../../components/Container/Container";
+import Navbar from "../../components/Navbar/Navbar"
 
 /* Recuperamos el Local Storage de las peliculas favoritas */
 let recupeStoragePelis = localStorage.getItem('favoritosPelis');
@@ -8,10 +10,8 @@ let favoritosPelis = JSON.parse(recupeStoragePelis);
 function Favoritos() {
     <React.Fragment>
         <Navbar/>
-        <h1>Home</h1>
-
-        <h3>Peliculas Favoritas</h3>
-
+        <h1>Favoritos</h1>
+        <Container/>
     </React.Fragment>
 }
 console.log(favoritosPelis);
