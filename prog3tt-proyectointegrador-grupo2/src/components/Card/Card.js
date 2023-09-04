62c5068b7b833066d12fa88406f07295a55d7627
lucioFreixas <lfreixas@udesa.edu.ar>
2023-09-04 15:14:12 -0300

    cards

---
+++ prog3tt-proyectointegrador-grupo2/src/components/Card/Card.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class Card extends Component {
+    constructor(){
+        super();
+    this.state = {
+        pelicula: {}
+    };
+    }
+    componentDidMount(){
+        fetch()
+        .then()
+        .then()
+        .catch()
+    }
+    
+
+    render() {
+        return <article></article>;
+    }
+}
+
+
+
+export default Card;
---
+++ prog3tt-proyectointegrador-grupo2/src/components/CardSerie/CardSerie.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Card from "../Card/Card";
+
+class CardSerie extends Component{
+    constructor(){
+        super();
+    }
+}
+
+export default CardSerie;
