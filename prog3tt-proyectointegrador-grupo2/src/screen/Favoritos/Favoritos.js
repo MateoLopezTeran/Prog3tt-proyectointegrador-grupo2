73ff5f545b0d7c700396cadcce2898f888e39333
lucioFreixas <lfreixas@udesa.edu.ar>
2023-09-04 16:14:01 -0300

    avance favs

--- prog3tt-proyectointegrador-grupo2/src/screen/Favoritos/Favoritos.js
+++ prog3tt-proyectointegrador-grupo2/src/screen/Favoritos/Favoritos.js
@@ -1,8 +1,18 @@
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+
 /* Recuperamos el Local Storage de las peliculas favoritas */
 let recupeStoragePelis = localStorage.getItem('favoritosPelis');
 console.log(recupeStoragePelis);
 
 let favoritosPelis = JSON.parse(recupeStoragePelis);
+function Favoritos() {
+    <React.Fragment>
+        <Navbar/>
+        <h1>Home</h1>
 
+        <h3>Peliculas Favoritas</h3>
+
+    </React.Fragment>
+}
 console.log(favoritosPelis);
 
