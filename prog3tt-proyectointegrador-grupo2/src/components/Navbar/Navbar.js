db59bcce083b6fc2d4bcd5b8e6b0b99fed447929
aaugier@udesa.edu.ar <aaugier@udesa.edu.ar>
2023-09-04 16:45:50 -0300

    navbar

---
+++ prog3tt-proyectointegrador-grupo2/src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="headerDerecha">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Favoritos">Favorites</Link>
+        </li>
+        <li>
+          <Link to="/Detail">Detail</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
+
+/* 
+<section class = "headerIzquierda">
+        <img class="logoheader" src="./img/LogoSolo.png" alt="Logo Empresa">
+        <a href="./home.html" class="nombrepagina"><h2 class="nombrepagina">Wawa Max</h2></a>
+    </section>
+
+    <nav class="headerDerecha">
+        <article class="linksHeader">
+            <a class="linkfavoritos" href="./favoritos.html">Favoritos</a>
+            <a class="linkgeneros" href="./generos.html">Géneros</a>
+        </article>
+        <form class="formbusqueda" action="./searchresults.html" method="get">
+            <input class="inputbusqueda" type="text" name="buscar" id="" placeholder="Película o Serie">
+            <button class="submitbtn" type="submit">Buscar</button>
+        </form>
+    </nav>
+
+ */
