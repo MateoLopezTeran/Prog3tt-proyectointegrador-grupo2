f9adf8aaded1e364efbe7975880af30164768c1e
lucioFreixas <lfreixas@udesa.edu.ar>
2023-09-04 15:32:58 -0300

    app

--- prog3tt-proyectointegrador-grupo2/src/App.js
+++ prog3tt-proyectointegrador-grupo2/src/App.js
@@ -1,4 +1,15 @@
+import React from "react";
+import CardSerie from "./components/CardSerie/CardSerie";
+import Card from "./components/Card/Card";
+import screen from "./screen";
+
+
 function App() {
+  const peliculas = [{
+    image: '',
+    name: '',
+    info: ''
+  }]
   return (
     <p>React</p>
   );
