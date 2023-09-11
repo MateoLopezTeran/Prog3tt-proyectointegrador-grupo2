ca6f3e5b49bb4f4a692313b0970b76b6356cd8c3
Mateo Lopez Teran <mlopezteran@udesa.edu.ar>
2023-09-11 17:41:56 -0300

    detalle

--- prog3tt-proyectointegrador-grupo2/src/screen/Detail/Detail.js
+++ prog3tt-proyectointegrador-grupo2/src/screen/Detail/Detail.js
@@ -23,11 +23,38 @@ class Detail extends Component{
     console.log(this.props);
     return (
     <React.Fragment>
-        <h2>{this.state.pelicula.title}</h2>
-        {/* <h2>{this.state.pelicula}</h2> */}
-        {this.state.pelicula ?
-        <img className="poster" src={`https://image.tmdb.org/t/p/w500/${this.state.pelicula.poster_path}`} alt="imagenPelicula" />
-        : <h2>cargando...</h2>}
+      <main className="detalle">
+        <section className="titulo_texto">
+          <h1 className="titulo">{this.state.pelicula.title}</h1>
+
+          <article className="texto_arriba_foto">
+            <p className="descripcion_arriba">{this.state.pelicula.release_date} - ⭐ {this.state.pelicula.vote_average}</p>
+          </article>
+        </section>
+        
+        <section className="contenido_principal">
+          <article className="imagen_detalle">
+            {this.state.pelicula ?
+            <img className="poster" src={`https://image.tmdb.org/t/p/w500/${this.state.pelicula.poster_path}`} alt="imagenPelicula" />
+            : <h2>cargando...</h2>}
+          </article>
+
+          <article className="texto_abajo_foto">
+            
+            <p className="descripcion_abajo">{this.state.pelicula.runtime} minutos</p>
+            <p className="descripcion_abajo">{this.state.pelicula.overview}</p>
+            {<p className="descripcion_abajo">Generos: {this.state.pelicula.genres.map((genres, i) => {
+              return (
+                genres.name
+              )
+          })}</p>}
+          </article>
+        </section>
+        
+        <p className="descripcion_abajo">
+          <a href="/Favoritos" class="link_favoritos" id="botonFavoritosPelis">+ Agregar a favoritos</a>
+        </p>
+      </main>
     </React.Fragment>
   );
 }}
