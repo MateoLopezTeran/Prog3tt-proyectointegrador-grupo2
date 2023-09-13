c8601c42b267b4090a2dc9e74a9296059988dc95
lucioFreixas <lfreixas@udesa.edu.ar>
2023-09-13 18:50:58 -0300

    buscador

--- prog3tt-proyectointegrador-grupo2/src/components/Formulario/Formulario.js
+++ prog3tt-proyectointegrador-grupo2/src/components/Formulario/Formulario.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class Formulario extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
           valorInput: '',
         };
